fd3e769f44ad3f1633f0606eb0dcea3dcb0f8ee8
Marisol <00094716@uca.edu.sv>
2020-05-22 15:49:40 -0600

    BottomNavigationBar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int currenteIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Home Page'),
-      ),
+      body: _callPage(currenteIndex),
+      bottomNavigationBar: _crearBottomNavigationBar(),
+    );
+  }
+
+  Widget _callPage(int paginaActual){
+    switch(paginaActual){
+      case 0:
+        return MapasPages();
+      case 1:
+        return DireccionesPage();
+
+      default:
+        return MapasPages();
+    }
+  }
+
+  //Navegacion entre pantallas
+  Widget _crearBottomNavigationBar(){
+    return BottomNavigationBar(
+      //Controlo las paginas con el int
+      currentIndex: currenteIndex,
+      //Funcion que cambia entre pantallas
+      onTap: (index){
+        setState(() {
+          currenteIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas')
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones')
+        ),
+      ]
     );
   }
 }
