fe411b9ca8e4296a3945acb16fa847208f93ecb8
Marisol <00094716@uca.edu.sv>
2020-09-22 02:32:17 -0600

    Actualizar registros

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -71,11 +71,37 @@ class DBProvider {
 
   //SELECT -- Obtener información
   Future<ScanModel> getScanId(int id) async {
-    
     final db = await database;
     final res = await db.query('Scans', where: 'id = ?', whereArgs: [id]);
 
     //La primera posicion
     return res.isNotEmpty ? ScanModel.fromJson(res.first) : null;
+  }
+
+  Future<List<ScanModel>> getTodosScans(int id) async {
+    final db = await database;
+    final res = await db.query('Scans');
+
+    List<ScanModel> list =
+        res.isNotEmpty ? res.map((c) => ScanModel.fromJson(c)).toList() : [];
+    return list;
+  }
+
+  Future<List<ScanModel>> getScansPorTipo(String tipo) async {
+    final db = await database;
+    final res = await db.rawQuery("SELECT * FROM Scans WHERE tipo = '$tipo'");
+
+    List<ScanModel> list =
+        res.isNotEmpty ? res.map((c) => ScanModel.fromJson(c)).toList() : [];
+    return list;
+  }
+
+  //Actualizar Registros
+  Future<int> updateScan(ScanModel nuevoScan) async {
+    final db = await database;
+    final res = await db.update('Scans', nuevoScan.toJson(),
+        where: 'id = ?', whereArgs: [nuevoScan.id]);
+
+    return res;
   }
 }
