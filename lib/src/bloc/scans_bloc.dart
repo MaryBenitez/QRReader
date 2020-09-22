fa23c15d9864aa92d838adcad6ffe0786a687aa2
Marisol <00094716@uca.edu.sv>
2020-09-22 13:16:54 -0600

    Stream Bloc

--- lib/src/bloc/scans_bloc.dart
+++ lib/src/bloc/scans_bloc.dart
@@ -11,6 +11,7 @@ class ScansBloc {
 
   ScansBloc._internal() {
     //Obtener Scans en la db
+    obtenerScans();
   }
 
   final _scansController = StreamController<List<ScanModel>>.broadcast();
@@ -19,5 +20,28 @@ class ScansBloc {
 
   dispose() {
     _scansController?.close();
+  }
+
+  //Obtener todos los scans
+  obtenerScans() async {
+    _scansController.sink.add(await DBProvider.db.getTodosScans());
+  }
+
+  //Agregar Scans
+  agregarScan(ScanModel scan) async {
+    await DBProvider.db.nuevoScan(scan);
+    obtenerScans();
+  }
+
+  //Borrar Scans
+  borrarScan(int id) {
+    DBProvider.db.deleteScan(id);
+    obtenerScans();
+  }
+
+  //Borrar todos los scans
+  borrarScanTodos() async {
+    await DBProvider.db.deleteAll();
+    obtenerScans();
   }
 }
