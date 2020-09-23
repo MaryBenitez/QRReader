a0571fa47dd8bb1585607f59f8519eac87d75ca5
Marisol <00094716@uca.edu.sv>
2020-09-22 20:20:19 -0600

    Arreglo en la vista del mapa

--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -3,25 +3,70 @@ import 'package:qrreaderapp/src/models/scan_model.dart';
 
 import 'package:flutter_map/flutter_map.dart';
 
-class MapaPages extends StatelessWidget {
+class MapaPages extends StatefulWidget {
+  @override
+  _MapaPagesState createState() => _MapaPagesState();
+}
+
+class _MapaPagesState extends State<MapaPages> {
   final map = new MapController();
+
+  String tipoMapa = 'streets-v11';
 
   @override
   Widget build(BuildContext context) {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Coordenadas QR'),
-          actions: [
-            IconButton(
-                icon: Icon(Icons.my_location),
-                onPressed: () {
-                  map.move(scan.getLatLng(), 15);
-                })
-          ],
-        ),
-        body: _crearFlutterMap(scan));
+      appBar: AppBar(
+        title: Text('Coordenadas QR'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.my_location),
+              onPressed: () {
+                map.move(scan.getLatLng(), 15);
+              })
+        ],
+      ),
+      body: _crearFlutterMap(scan),
+      floatingActionButton: _crearBotonFlotante(context, scan),
+    );
+  }
+
+  Widget _crearBotonFlotante(BuildContext context, ScanModel scan) {
+    return FloatingActionButton(
+        child: Icon(Icons.repeat),
+        backgroundColor: Theme.of(context).primaryColor,
+        onPressed: () {
+          //streets, dark, light, outdoors, satellite
+          if (tipoMapa == 'streets-v11') {
+            tipoMapa = 'dark-v10';
+            print('SUPUESTAMENTE SE CAMBIO A DARK');
+          } else if (tipoMapa == 'dark-v10') {
+            tipoMapa = 'light-v10';
+            print('SUPUESTAMENTE SE CAMBIO A LIGHT');
+          } else if (tipoMapa == 'light-v10') {
+            tipoMapa = 'outdoors-v11';
+            print('SUPUESTAMENTE SE CAMBIO A OUTDOOR');
+          } else if (tipoMapa == 'outdoors-v11') {
+            tipoMapa = 'satellite-v9';
+            print('SUPUESTAMENTE SE CAMBIO A SATELITE');
+          } else {
+            tipoMapa = 'streets-v11';
+            print('SUPUESTAMENTE SE CAMBIO AL PRIMERO');
+          }
+
+          //Hace el cambio en pantalla
+          setState(() {});
+
+          //movimiento #1 al maximo de zoom
+          map.move(scan.getLatLng(), 30);
+
+          //Regreso al Zoom Deseado después de unos Milisegundos
+          Future.delayed(Duration(milliseconds: 50), () {
+            map.move(scan.getLatLng(), 15);
+          });
+        });
   }
 
   Widget _crearFlutterMap(ScanModel scan) {
@@ -42,7 +87,7 @@ class MapaPages extends StatelessWidget {
         additionalOptions: {
           'accessToken':
               'pk.eyJ1IjoibWFyeWJlIiwiYSI6ImNrZmVpM2NjNTAyYjEzNm9ibmtmOXpibTYifQ.OolHeAr-0pdxPVmP8P0aHA',
-          'id': 'mapbox/streets-v11'
+          'id': 'mapbox/$tipoMapa'
         });
   }
 
